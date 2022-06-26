f91b214e641e62c9c86d543517a572aed6388a49
Akash <akashuhulekal@gmail.com>
2022-06-26 23:20:18 +0530

    started with frontend createcontext and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
-   <h1>djsdsjdnndn</h1>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route  path="/" element={<Home />}/>
+          <Route  path="/about" element={ <About />}/>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
